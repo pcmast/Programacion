0f174f7a3cf92b79479e1cebe5b08184c67887be
Pedro <pedroneo2005@gmail.com>
2025-03-24 14:31:04 +0100

    Creada clase utilidades

---
+++ src/main/java/model/Creador.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Creador extends Usuario{
+    private String ongPertenece;
+
+    public Creador(String nombre, String usuario, String contrasenna, String correo) {
+        super(nombre, usuario, contrasenna, correo);
+    }
+
+
+
+}
