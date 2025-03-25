59d2cde3494723fd59d427e693000a10e9e6b707
alejandroo24 <alejandro.j.morenodiaz@gmail.com>
2025-03-25 13:40:58 +0100

    Actividadcontroller con metodos crearActividad,eliminarActividad y verActividades

--- src/main/java/controller/ActividadController.java
+++ src/main/java/controller/ActividadController.java
@@ -5,15 +5,19 @@ import model.EstadoActividad;
 import utils.Utilidades;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class ActividadController {
 
+    ArrayList<Actividad> listaActividades = new ArrayList<Actividad>();
+
     /**
      * Metodo que crea una actividad nueva pidiendo los datos de la actividad
-     * @return true si la actividad se creó correctamente, false en caso contrario.
      *
+     * @return true si la actividad se creó correctamente, false en caso contrario.
      */
-    public static boolean creaActividad() {
+    public boolean creaActividad() {
+
         boolean actividadCreada = false;
 
         Utilidades.mostrarMensaje("Ingrese los datos de la actividad:");
@@ -24,7 +28,7 @@ public class ActividadController {
         Utilidades.mostrarMensaje("Seleccione el estado de la actividad:\n" +
                 "1. NO INICIADA\n" +
                 "2. EN PROCESO\n" +
-                "3. COMPLETADA" );
+                "3. COMPLETADA");
         int opcion = Utilidades.leeEntero("Opción:");
         EstadoActividad estadoActividad = null;
         switch (opcion) {
@@ -41,10 +45,51 @@ public class ActividadController {
 
         Actividad actividad = new Actividad(nombreActividad, descripcionActividad, fechaInicio, fechaFin, false, estadoActividad);
         // Llenar los datos de la actividad
-        if (actividad!= null) {
+        if (actividad != null) {
             actividadCreada = true;
             Utilidades.mostrarMensaje("Actividad creada con éxito");
+            listaActividades.add(actividad);
         }
         return actividadCreada;
     }
+
+    /**
+     * Metodo que primero comprueba que la lista de actividades no este vacia y despues muestra la lista de actividades. De esa lista se introduce el nomnbre
+     * de la actividad que se quiere eliminar y si esta actividad está en la lista de actividades se elimina.
+     *
+     * @return true si la actividad se ha eliminado correctamente, false en caso contrario.
+     *
+     */
+    public boolean eliminaActividad() {
+        boolean actividadEliminada = false;
+        boolean listaVacia = listaActividades.isEmpty();
+        if (listaActividades.isEmpty()) {
+            Utilidades.mostrarMensaje("No hay actividades creadas");
+            return actividadEliminada;
+        } else {
+            Utilidades.mostrarMensaje("Introduce el nombre de la actividad a eliminar:  ");
+            for (Actividad actividadActual : listaActividades) {
+                Utilidades.mostrarMensaje(actividadActual.toString());
+            }
+
+            String opcion = Utilidades.pideString("Opción:");
+            for (Actividad actividadActual : listaActividades) {
+                if (actividadActual.getNombre().equalsIgnoreCase(opcion)) {
+                    listaActividades.remove(actividadActual);
+                    actividadEliminada = true;
+                    Utilidades.mostrarMensaje("Actividad eliminada con éxito");
+                    break;
+                }else{
+                    Utilidades.mostrarMensaje("Actividad no encontrada");
+                }
+            }
+            return actividadEliminada;
+        }
+    }
+    public void muestraActividades() {
+        for (Actividad actividadActual : listaActividades){
+            Utilidades.mostrarMensaje(actividadActual.toString());
+        }
+    }
 }
+
