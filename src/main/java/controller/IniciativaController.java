f47d1724a4cbbdccdf786c089c8fd6fcdffc55e4
Pedro <pedroneo2005@gmail.com>
2025-04-01 18:25:30 +0200

    Corregir el codigo en general

--- src/main/java/controller/IniciativaController.java
+++ src/main/java/controller/IniciativaController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import dataAcces.XMLManager;
 import model.*;
 import utils.Utilidades;
 import view.*;
@@ -15,135 +14,72 @@ public class IniciativaController {
      *
      */
     public void creaIniciativa() {
-        try {
-            ArrayList<Iniciativa> iniciativasDesdeXML = XMLManager.readXML(new ArrayList<>(), "iniciativas.xml");
-            if (iniciativasDesdeXML != null) {
-                Creador creador = (Creador) usuarioActualController.getUsuario();
-                creador.setList(iniciativasDesdeXML);
-            }
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-
         Creador creador = (Creador) usuarioActualController.getUsuario();
-        Iniciativa iniciativa = MenuIniciativaActividad.pideDatosCrearIniciativa(creador);
+        Iniciativa iniciativa = menuIniciativaActividad.pideDatosCrearIniciativa(creador);
         boolean creado = creador.crearIniciativa(iniciativa);
-
-        if (creado) {
-            MenuVista.muestraMensaje("Se ha creado correctamente.");
-            try {
-                XMLManager.writeXML(creador.verIniciativas(), "iniciativas.xml");
-            } catch (RuntimeException e) {
-                e.printStackTrace();
-            }
-        } else {
-            MenuVista.muestraMensaje("No se ha podido crear la iniciativa.");
+        if (creado){
+            MenuVista.muestraMensaje("Se ha creado correctamente");
+        }else {
+            MenuVista.muestraMensaje("No se ha podido crear la iniciativa");
         }
     }
-
 
     /** por corregir
      * Método que elimina una iniciativa. (por ahora solo la deja en null)
      * @param
      */
     public void eliminaIniciativa() {
-        try {
-            ArrayList<Iniciativa> iniciativasDesdeXML = XMLManager.readXML(new ArrayList<>(), "iniciativas.xml");
-            if (iniciativasDesdeXML != null) {
-                Creador creador = (Creador) usuarioActualController.getUsuario();
-                creador.setList(iniciativasDesdeXML);
-            }
-        } catch (RuntimeException e) {
-            e.printStackTrace(); // Registrar error sin interacción con el usuario.
-        }
-
         boolean eliminado = false;
         Creador creador = (Creador) usuarioActualController.getUsuario();
         ArrayList<Iniciativa> list = creador.verIniciativas();
         String nombre = Utilidades.pideString("Introduce el nombre de la iniciativa");
-
-        for (Iniciativa iniciativa : list) {
-            eliminado = iniciativa.eliminarList(nombre);
+        for (Iniciativa iniciativa:list){
+          eliminado = iniciativa.eliminarList(nombre);
         }
-
-        if (eliminado) {
-            MenuVista.muestraMensaje("Se ha eliminado correctamente.");
-            try {
-                XMLManager.writeXML(creador.verIniciativas(), "iniciativas.xml");
-            } catch (RuntimeException e) {
-                e.printStackTrace();
-            }
-        } else {
-            MenuVista.muestraMensaje("No se ha podido eliminar la iniciativa.");
+        if (eliminado){
+            MenuVista.muestraMensaje("Se ha eliminado correctamente");
+        }else {
+            MenuVista.muestraMensaje("No se ha podido eliminar la iniciativa");
         }
     }
-
 
     /**
      * Método que modifica una iniciativa pidiendo al usuario que cree una nueva con los datos actualizados y eliminando la anterior
      *
      */
-    public void modificarIniciativa() {
-        try {
-            ArrayList<Iniciativa> iniciativasDesdeXML = XMLManager.readXML(new ArrayList<>(), "iniciativas.xml");
-            if (iniciativasDesdeXML != null) {
-                Creador creador = (Creador) usuarioActualController.getUsuario();
-                creador.setList(iniciativasDesdeXML);
-            }
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-
-        // Modificar iniciativa.
+    public void modificarIniciativa(){
         boolean actualizado = false;
         Creador creador = (Creador) usuarioActualController.getUsuario();
         ArrayList<Iniciativa> list = creador.verIniciativas();
-        Iniciativa iniciativa = MenuIniciativaActividad.pideDatosCrearIniciativa(creador);
-
-        for (Iniciativa iniciativa1 : list) {
-            actualizado = iniciativa1.modificar(iniciativa);
+        Iniciativa iniciativa = menuIniciativaActividad.pideDatosCrearIniciativa(creador);
+        for (Iniciativa iniciativa1:list){
+         actualizado = iniciativa1.modificar(iniciativa);
+        }
+        if (actualizado){
+            MenuVista.muestraMensaje("Se ha podido modificar correctamente");
+        }else {
+            MenuVista.muestraMensaje("No se ha podido modificar no existe la iniciativa");
         }
 
-        if (actualizado) {
-            MenuVista.muestraMensaje("Se ha podido modificar correctamente.");
-            try {
-                XMLManager.writeXML(creador.verIniciativas(), "iniciativas.xml");
-            } catch (RuntimeException e) {
-                e.printStackTrace();
-            }
-        } else {
-            MenuVista.muestraMensaje("No se ha podido modificar la iniciativa.");
-        }
+
     }
-
 
     /**
      * Método que muestra todas las iniciativas de un creador
      *
      */
     public void muestraIniciativas() {
-        try {
-            ArrayList<Iniciativa> iniciativasDesdeXML = XMLManager.readXML(new ArrayList<>(), "iniciativas.xml");
-            if (iniciativasDesdeXML != null) {
-                Creador creador = (Creador) usuarioActualController.getUsuario();
-                creador.setList(iniciativasDesdeXML);
-            }
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-
         Creador creador = (Creador) usuarioActualController.getUsuario();
         ArrayList<Iniciativa> list = creador.verIniciativas();
 
-        for (Iniciativa iniciativa : list) {
-            MenuIniciativaActividad.muestraObjeto(iniciativa);
-            ArrayList<Actividad> list1 = iniciativa.getList();
-            for (Actividad actividad : list1) {
-                MenuIniciativaActividad.muestraObjeto(actividad);
-            }
+        for (Iniciativa iniciativa:list){
+        menuIniciativaActividad.muestraObjeto(iniciativa);
+        ArrayList<Actividad> list1 =  iniciativa.getList();
+        for (Actividad actividad: list1){
+            menuIniciativaActividad.muestraObjeto(actividad);
+        }
         }
     }
-
     public void muestraIniciativasNombre(){
         Creador creador = (Creador) usuarioActualController.getUsuario();
         ArrayList<Iniciativa> list = creador.verIniciativas();
@@ -156,7 +92,7 @@ public class IniciativaController {
         Voluntario voluntario = (Voluntario) usuarioActualController.getUsuario();
         ArrayList<Actividad> list = voluntario.getList();
         for (Actividad actividad:list){
-            MenuIniciativaActividad.muestraObjeto(actividad);
+            menuIniciativaActividad.muestraObjeto(actividad);
         }
 
     }
